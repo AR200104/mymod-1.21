0ec4d4c261749097b5607380255fef24871cc782
ar <andreas.romanov@gmail.com>
2024-10-24 11:18:42 +0200

    Initial commit

---
+++ src/main/java/net/mymod121/MyModClient.java
@@ -0,0 +1,10 @@
+package net.mymod121;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
